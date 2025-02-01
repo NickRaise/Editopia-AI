299caa419afc34936c95b9839a7585cbc993ee1e
NickRaise <nikhilnainwaya17@gmail.com>
2025-02-01 17:29:54 +0530

    updated fonts in tailwind

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AuthenticationPage = () => {
+  return (
+    <main className="h-screen grid grid-cols-2 relative">
+          <div>
+              Image
+          </div>
+      <div>Login form</div>
+    </main>
+  );
+};
+
+export default AuthenticationPage;
